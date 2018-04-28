764088b458089bf92b218a839eb1aed800a162b6
fabio.moratti <fabio.moratti@nexse.com>
2018-04-28 22:47:47 +0200

    base file

---
+++ src/main/java/com/stepob/life/App.java
@@ -0,0 +1,13 @@
+package com.stepob.life;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
